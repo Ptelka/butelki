063eb9e8b2b19d1ff44c8e617622c3f13b588d66
Piotr Telka <piotr.telka1@gmail.com>
2019-09-07 17:19:58 +0200

    asddasasd

---
+++ Assets/Scripts/ScreenSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSelector : Clickable
+{
+    public GameObject screen;
+    
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public void OnClick()
+    {
+        var pos = Camera.main.transform.position;
+        pos.x = screen.transform.position.x;
+        pos.y = screen.transform.position.y;
+        Camera.main.transform.position = pos;
+    }
+}
